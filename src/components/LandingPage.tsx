d209f7f6f3963d0d7a1ee6cbd1794a2caf43f390
Daryl Reyes <drylrys08@gmail.com>
2024-01-03 18:37:57 +0800

    landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,16 +4,16 @@ import React from "react";
 function LandingPage() {
   const bg =
     process.env.NODE_ENV === "development"
-      ? `bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url("/assets/img/cargo3.jpg")]`
-      : `bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url("https://drylrys08.github.io/erth-liner/assets/img/cargo3.jpg")]`;
+      ? `bg-[linear-gradient(to_bottom,rgba(0,0,0,1),rgba(0,0,0,0)),url("/assets/img/cargo3.jpg")]`
+      : `bg-[linear-gradient(to_bottom,rgba(0,0,0,1),rgba(0,0,0,0)),url("https://drylrys08.github.io/erth-liner/assets/img/cargo3.jpg")]`;
 
   return (
     <div
       className={`h-[calc(100vh-100px)] ${bg}
-        bg-cover bg-no-repeat bg-top shadow`}
+        bg-cover bg-no-repeat bg-top `}
     >
-      <div className="h-full flex justify-end pb-36 flex-col gap-2 pl-2 lg:pl-10">
-        <h1 className="text-stroke font-extrabold  text-6xl md:text-8xl   text-[#FFFFFF] animate-fade-right animate-once animate-duration-[2000ms]">
+      <div className="h-full flex justify-center pb-36 flex-col gap-2 pl-2 lg:pl-10">
+        <h1 className="text-stroke font-extrabold text-6xl md:text-8xl   text-[#FFFFFF] animate-fade-right animate-once animate-duration-[2000ms]">
           Custom Solutions
         </h1>
         <h1 className="text-stroke font-extrabold  text-6xl md:text-8xl    text-[#FFFFFF] animate-fade-right animate-once animate-duration-[2000ms]">
