fb26e56ede81392c084419eb6f57fef1337de5db
Daryl Reyes <drylrys08@gmail.com>
2024-01-09 16:25:11 +0800

    fixfb messenger

--- src/components/FacebookMsg.tsx
+++ src/components/FacebookMsg.tsx
@@ -4,7 +4,11 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 function FacebookMsg() {
   return (
     <FacebookProvider appId="734832954918827" chatSupport>
-      <CustomChat pageId="104347214616405" minimized={true} />
+      <CustomChat
+        pageId="104347214616405"
+        minimized={true}
+        themeColor="black"
+      />
     </FacebookProvider>
   );
 }
