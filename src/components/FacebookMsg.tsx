0f08fb3342e6e4eab257de23c40dba9f6b3e34b0
Daryl Reyes <drylrys08@gmail.com>
2024-01-09 18:22:30 +0800

    fix fb messenger

--- src/components/FacebookMsg.tsx
+++ src/components/FacebookMsg.tsx
@@ -4,11 +4,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 function FacebookMsg() {
   return (
     <FacebookProvider appId="734832954918827" chatSupport>
-      <CustomChat
-        pageId="104347214616405"
-        minimized={true}
-        themeColor="black"
-      />
+      <CustomChat pageId="104347214616405" minimized={true} />
     </FacebookProvider>
   );
 }
